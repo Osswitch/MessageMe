be36a48e59db452f8513d710a05cdf0db52ca3c3
Xinyao Zhang <xyzhang0817@gmail.com>
2019-07-15 14:48:16 -0400

    Add auto scroll.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,11 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function() {
+	if ($('#messages').length > 0) {
+		$('#messages').scrollTop($('#messages')[0].scrollHeight);
+	}
+}
 
 $(document).on('turbolinks:load', function() {
 	$('.ui.dropdown')
@@ -29,4 +34,6 @@ $(document).on('turbolinks:load', function() {
       .transition('fade')
     ;
   });
+
+  scroll_bottom();
 })
